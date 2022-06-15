4face259e626e7fef3b8145afe70cdd88b49ed1d
1588mobalmobal <43930523+1588mobalmobal@users.noreply.github.com>
2022-06-15 22:45:00 +0900

    photos_seed

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -1,5 +1,5 @@
 import random
-from secrets import choice
+from django.contrib.admin.utils import flatten
 from django.core.management.base import BaseCommand
 from django_seed import Seed
 from users import models as user_models
@@ -24,9 +24,24 @@ class Command(BaseCommand):
             room_models.Room,
             number,
             {
+                "name": lambda x: seeder.faker.address(),
                 "host": lambda x: random.choice(all_users),
                 "room_type": lambda x: random.choice(room_types),
+                "price": lambda x: random.randint(100, 1000),
+                "guest": lambda x: random.randint(1, 10),
+                "beds": lambda x: random.randint(1, 10),
+                "bedrooms": lambda x: random.randint(1, 10),
+                "baths": lambda x: random.randint(1, 10),
             },
         )
-        seeder.execute()
+        created_photos = seeder.execute()
+        created_clean = flatten(list(created_photos.values()))
+        for serial in created_clean:
+            room = room_models.Room.objects.get(id=serial)
+            for i in range(3, random.randint(6, 9)):
+                room_models.Photo.objects.create(
+                    caption=seeder.faker.sentence(),
+                    file=f"/room_photos/{random.randint(1,31)}.webp",
+                    room=room,
+                )
         self.stdout.write(self.style.SUCCESS(f"{number} rooms was created!"))
